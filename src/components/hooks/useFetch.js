917efa6bf59f6b1c2d4bd92230dd22f42d4d57c8
Ear sokchan <145833822+Sokchan3434456677@users.noreply.github.com>
2025-01-08 06:21:43 -0300

    useFetch.js

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -3,25 +3,36 @@ import { useState, useEffect } from 'react';
 const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const response = await fetch(url);
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Failed to fetch data');
         }
-        const data = await response.json();
-        setData(data);
+        const result = await response.json();
+        setData(result);
+        setError(null); // Clear error if data fetch is successful
       } catch (error) {
-        setError(error);
+        if (error.name === 'TypeError') {
+          // Network error, likely no internet connection
+          setError('No internet connection. Please check your network.');
+        } else {
+          // General error
+          setError('Error fetching products. Please try again later.');
+        }
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [url]);
 
-  return { data, error };
+  return { data, error, loading };
 };
 
 export default useFetch;
